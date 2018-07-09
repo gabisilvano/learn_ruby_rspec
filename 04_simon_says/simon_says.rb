aa25f306b837edfbf75e343354094c7acf26f7b3
gabisilvano <gabrielle.silvano@gmail.com>
2018-07-09 19:44:57 +0200

    5 exo

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,38 @@
 #write your code here
+def echo (a)
+	
+	a
+	
+end
+
+def shout (a)
+	a.upcase
+end
+
+def repeat (a,b=2)
+ 	c = (a + " ")*(b-1)
+ 	c + a
+	
+end
+
+def start_of_word (a,i)
+	a[0..i-1]
+end
+
+def first_word (a)
+
+	i = 0
+	while a[i] != " "
+		i+=1
+	end
+	i-=1
+	a[0..i]	
+end
+def titleize (a)
+	a = a.split.map(&:capitalize).join(' ')
+	#if a.split.map { |e| e.length <=3  }
+	#	a = a.downcase
+	#end
+
+
+end
--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,1 +1,38 @@
 #write your code here
+def time_string (a)
+	if a.to_i <= 60
+		if a.to_i <= 10
+			a = "0" + a.to_s
+			"00:00:#{a}"
+		else
+		"00:00:#{a.to_s}"
+		end
+	elsif ((a.to_i/60) <= 60) && ((a.to_i/60) >= 1)
+		b = a.to_i/60
+		c = a.to_i%60
+		if b <= 10 
+			b = "0" + b.to_s  
+		end
+		if c <= 10 
+			c = "0" + c.to_s
+		end
+		"00:#{b}:#{c}"
+	else 
+		d = a.to_i/3600
+		b = a.to_i - 3600
+		b = b.to_i/60
+		c = a.to_i%60
+		if b <= 10 
+			b = "0" + b.to_s  
+		end
+		if c <= 10 
+			c = "0" + c.to_s
+		end
+		if d <= 10 
+			d = "0" + d.to_s
+		end
+		"#{d}:#{b}:#{c}"
+
+	end
+
+end
